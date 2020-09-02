5438cf9f7e2c983df5d8a143f8e72e63dadef2e0
Sota Nishiyama <2480souta@gmail.com>
2020-09-02 15:53:19 +0900

    Added suffix_array

---
+++ string/suffix_array.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct SuffixArray {
+    vector<int> sa;
+    const string s;
+
+    SuffixArray(const string& str) : s(str) {
+        int n = s.size();
+        sa.resize(n);
+        iota(sa.begin(), sa.end(), 0);
+        sort(sa.begin(), sa.end(), [&](int i, int j) {
+            return s[i] == s[j] ? i > j : s[i] < s[j];
+        });
+        int cl = 0;
+        vector<int> rank(n);
+        for (int i = 1; i < n; i++) {
+            if (s[sa[i-1]] != s[sa[i]]) cl++;
+            rank[sa[i]] = cl;
+        }
+        vector<int> tmp(n), nrank(n), cnt(n);
+        for (int k = 1; k < n; k *= 2) {
+            for (int i = 0; i < n; i++) {
+                tmp[i] = sa[i] - k;
+                if (tmp[i] < 0) tmp[i] += n;
+            }
+            fill(cnt.begin(), cnt.end(), 0);
+            for (int i = 0; i < n; i++) cnt[rank[tmp[i]]]++;
+            for (int i = 1; i < n; i++) cnt[i] += cnt[i-1];
+            for (int i = n - 1; i >= 0; i--) sa[--cnt[rank[tmp[i]]]] = tmp[i];
+            nrank[sa[0]] = 0;
+            cl = 0;
+            for (int i = 1; i < n; i++) {
+                if (rank[sa[i-1]] != rank[sa[i]] || rank[(sa[i-1] + k) % n] != rank[(sa[i] + k) % n]) {
+                    cl++;
+                }
+                nrank[sa[i]] = cl;
+            }
+            swap(rank, nrank);
+        }
+    }
+
+    int operator[](int k) const {
+        return sa[k];
+    }
+
+    bool lt_substr(const string& t, int si = 0, int ti = 0) {
+        int sn = s.size(), tn = t.size();
+        while (si < sn && ti < tn) {
+            if (s[si] < t[ti]) return true;
+            if (s[si] > t[ti]) return false;
+            si++;
+            ti++;
+        }
+        return si >= sn && ti < tn;
+    }
+
+    int lower_bound(const string& t) {
+        int lb = -1, ub = sa.size();
+        while (ub - lb > 1) {
+            int m = (lb + ub) / 2;
+            if (lt_substr(t, sa[m])) lb = m;
+            else ub = m;
+        }
+        return ub;
+    }
+
+    int upper_bound(string& t) {
+        t.back()++;
+        int i = lower_bound(t);
+        t.back()--;
+        return i;
+    }
+};
---
+++ test/aoj/ALDS1_14_B.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_14_B"
+
+#include "../../string/suffix_array.cpp"
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    string T;
+    cin >> T;
+    string P;
+    cin >> P;
+    SuffixArray sa(T + '$');
+    int lb = sa.lower_bound(P);
+    int ub = sa.upper_bound(P);
+    vector<int> ans;
+    for (int i = lb; i < ub; i++) ans.push_back(sa[i]);
+    sort(ans.begin(), ans.end());
+    for (int i : ans) cout << i << "\n";
+}
