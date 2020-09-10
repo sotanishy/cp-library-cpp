3ac5d8fb4395199a686b70715abc0a9c95291647
Sota Nishiyama <2480souta@gmail.com>
2020-09-11 01:14:21 +0900

    Made FFT and NTT faster

--- math/fft.cpp
+++ math/fft.cpp
@@ -5,20 +5,48 @@ struct FFT {
 private:
     inline static const double PI = acos(-1);
 
-    static void dft(vector<complex<double>>& f, int sign) {
-        int n = f.size();
-        if (n == 1) return;
-        vector<complex<double>> a, b;
-        for (int i = 0; i < n / 2; i++) {
-            a.push_back(f[2 * i]);
-            b.push_back(f[2 * i + 1]);
+    struct C {
+        double real, imag;
+        C() : real(0), imag(0) {}
+        C(double real, double imag) : real(real), imag(imag) {}
+        C operator+(const C& c) { return C(real + c.real, imag + c.imag); }
+        C operator-(const C& c) { return C(real - c.real, imag - c.imag); }
+        C operator*(const C& c) { return C(real * c.real - imag * c.imag, real * c.imag + imag * c.real); }
+    };
+
+    static void ufft(vector<C>& a) {
+        int n = a.size();
+        for (int m = n; m > 1; m >>= 1) {
+            double ang = 2.0 * PI / m;
+            C omega(cos(ang), sin(ang));
+            for (int s = 0; s < n / m; s++) {
+                C w(1, 0);
+                for (int i = 0; i < m / 2; i++) {
+                    C l = a[s * m + i];
+                    C r = a[s * m + i + m / 2];
+                    a[s * m + i] = l + r;
+                    a[s * m + i + m / 2] = (l - r) * w;
+                    w = w * omega;
+                }
+            }
         }
-        dft(a, sign);
-        dft(b, sign);
-        complex<double> cur = 1, zeta = polar(1.0, sign * 2.0 * PI / n);
-        for (int i = 0; i < n; i++) {
-            f[i] = a[i % (n / 2)] + cur * b[i % (n / 2)];
-            cur *= zeta;
+    }
+
+    static void iufft(vector<C>& a) {
+        int n = a.size();
+        for (int m = 2; m <= n; m <<= 1) {
+            double ang = -2.0 * PI / m;
+            C omega(cos(ang), sin(ang));
+            for (int s = 0; s < n / m; s++) {
+                C w(1, 0);
+                for (int i = 0; i < m / 2; i++) {
+                    C l = a[s * m + i];
+                    C r = a[s * m + i + m / 2] * w;
+                    a[s * m + i] = l + r;
+                    a[s * m + i + m / 2] = l - r;
+                    w = w * omega;
+                }
+            }
         }
     }
 
@@ -28,17 +56,15 @@ public:
         int size = a.size() + b.size() - 1;
         int n = 1;
         while (n < size) n <<= 1;
-        vector<complex<double>> na(n), nb(n);
-        for (int i = 0; i < a.size(); i++) {
-            na[i] = a[i];
-            nb[i] = b[i];
-        }
-        dft(na, 1);
-        dft(nb, 1);
-        for (int i = 0; i < n; i++) na[i] *= nb[i];
-        dft(na, -1);
+        vector<C> na(n), nb(n);
+        for (int i = 0; i < a.size(); i++) na[i].real = a[i];
+        for (int i = 0; i < b.size(); i++) nb[i].real = b[i];
+        ufft(na);
+        ufft(nb);
+        for (int i = 0; i < n; i++) na[i] = na[i] * nb[i];
+        iufft(na);
         vector<double> ret(size);
-        for (int i = 0; i < size; i++) ret[i] = na[i].real() / n;
+        for (int i = 0; i < size; i++) ret[i] = na[i].real / n;
         return ret;
     }
 };
