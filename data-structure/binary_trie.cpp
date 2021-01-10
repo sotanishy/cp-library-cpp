8d79436aff4289097454116258402910234658de
Sota Nishiyama <2480souta@gmail.com>
2021-01-11 02:35:25 +0900

    updated binary trie

--- data-structure/binary_trie.cpp
+++ data-structure/binary_trie.cpp
@@ -9,10 +9,10 @@
 template <typename T, int B = 32>
 class BinaryTrie {
 public:
-    BinaryTrie() : node(1) {}
+    BinaryTrie() : root(std::make_unique<Node>()) {}
 
     int size() const {
-        return node[0].count;
+        return root->count;
     }
 
     bool empty() const {
@@ -20,79 +20,105 @@ public:
     }
 
     int count(T x) const {
-        int t = 0;
-        for (int i = B - 1; i >= 0; --i) {
-            bool b = x >> i & 1;
-            if (node[t].ch[b] == -1) return 0;
-            t = node[t].ch[b];
-        }
-        return node[t].count;
+        return count(root, x, B - 1);
+    }
+
+    int count_less(T x, T xor_val = 0) const {
+        return count_less(root, x, xor_val, B - 1);
     }
 
     void insert(T x) {
-        int t = 0;
-        for (int i = B - 1; i >= 0; --i) {
-            bool b = x >> i & 1;
-            if (node[t].ch[b] == -1) {
-                node[t].ch[b] = node.size();
-                node.emplace_back();
-            }
-            ++node[t].count;
-            t = node[t].ch[b];
-        }
-        ++node[t].count;
+        insert(root, x, B - 1);
     }
 
     void erase(T x) {
-        if (!count(x)) return;
-        int t = 0;
-        for (int i = B - 1; i >= 0; --i) {
-            --node[t].count;
-            t = node[t].ch[x >> i & 1];
-        }
-        --node[t].count;
+        erase(root, x, B - 1);
     }
 
-    T min_element(T x = 0) const {
-        assert(size());
-        int t = 0;
-        T ret = 0;
-        for (int i = B - 1; i >= 0; --i) {
-            bool b = x >> i & 1;
-            int c = node[t].ch[b];
-            if (c != -1 && node[c].count > 0) {
-                t = c;
-            } else {
-                t = node[t].ch[1 - b];
-                ret |= T(1) << i;
-            }
-        }
-        return ret;
+    T min_element(T xor_val = 0) const {
+        assert(!empty());
+        return min_element(root, xor_val, B - 1);
     }
 
-    T max_element(T x = 0) const {
-        assert(size());
-        int t = 0;
-        T ret = 0;
-        for (int i = B - 1; i >= 0; --i) {
-            bool b = x >> i & 1;
-            int c = node[t].ch[1 - b];
-            if (c != -1 && node[c].count > 0) {
-                t = c;
-                ret |= T(1) << i;
-            } else {
-                t = node[t].ch[b];
-            }
-        }
-        return ret;
+    T max_element(T xor_val = 0) const {
+        assert(!empty());
+        return max_element(root, xor_val, B - 1);
     }
 
 private:
+    struct Node;
+    using node_ptr = std::unique_ptr<Node>;
+
     struct Node {
-        std::vector<int> ch;
-        int count;
-        Node() : ch(2, -1), count(0) {}
+        node_ptr ch[2];
+        int count = 0;
     };
 
-    std::vector<Node> node;
+    const node_ptr root;
+
+    int count(const node_ptr& t, T x, int k) const {
+        if (k == -1) return t->count;
+        bool b = x >> k & 1;
+        return t->ch[b] ? count(t->ch[b], x, k - 1) : 0;
+    }
+
+    int count_less(const node_ptr& t, T x, T xor_val, int k) const {
+        if (k == -1) return 0;
+        bool b = x >> k & 1;
+        bool f = xor_val >> k & 1;
+        int ret = 0;
+        if (f ^ b) {
+            if (t->ch[f]) ret += t->ch[f]->count;
+            if (t->ch[1 - f]) ret += count_less(t->ch[1 - f], x, xor_val, k - 1);
+        } else {
+            if (t->ch[f]) ret += count_less(t->ch[f], x, xor_val, k - 1);
+        }
+        return ret;
+    }
+
+    void insert(const node_ptr& t, T x, int k) {
+        if (k == -1) {
+            ++t->count;
+            return;
+        }
+        bool b = x >> k & 1;
+        if (!t->ch[b]) t->ch[b] = std::make_unique<Node>();
+        ++t->count;
+        insert(t->ch[b], x, k - 1);
+    }
+
+    void erase(const node_ptr& t, T x, int k) {
+        if (k == -1) {
+            --t->count;
+            return;
+        }
+        --t->count;
+        erase(t->ch[x >> k & 1]);
+    }
+
+    T min_element(const node_ptr& t, T xor_val, int k) const {
+        if (k == -1) return 0;
+        bool b = xor_val >> k & 1;
+        T ret = 0;
+        if (t->ch[b] && t->ch[b].count > 0) {
+            ret += min_element(t->ch[b], xor_val, k - 1);
+        } else {
+            ret += T(1) << k;
+            ret += min_element(t->ch[1 - b], xor_val, k - 1);
+        }
+        return ret;
+    }
+
+    T max_element(const node_ptr& t, T xor_val, int k) const {
+        if (k == -1) return 0;
+        bool b = xor_val >> k & 1;
+        T ret = 0;
+        if (t->ch[1 - b] && t->ch[1 - b].count > 0) {
+            ret += T(1) << k;
+            ret += max_element(t->ch[1 - b], xor_val, k - 1);
+        } else {
+            ret += max_element(t->ch[b], xor_val, k - 1);
+        }
+        return ret;
+    }
 };
