1811ac87002745f3dd919264ec7ec9605f379bbc
Sota Nishiyama <2480souta@gmail.com>
2021-01-11 18:15:22 +0900

    Updated layout

--- .verify-helper/docs/static/assets/js/hide_pagelink.js
+++ .verify-helper/docs/static/assets/js/hide_pagelink.js
@@ -1,3 +1,9 @@
-window.onload = function () {
-    document.getElementsByClassName('trigger')[0].style.display = 'none';
-};
+window.addEventListener('load', function(){
+    // add a link to GitHub in the header
+    let link = document.getElementsByClassName('page-link')[0];
+    link.href = 'https://github.com/sotanishy/competitive-programming-library';
+    link.textContent = 'GitHub';
+
+    // remove footer
+    document.getElementsByTagName('footer')[0].style.display = 'none';
+});
