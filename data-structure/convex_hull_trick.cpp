144bf73763358ab71dfc3a2d96b376d94a925b35
Sota Nishiyama <2480souta@gmail.com>
2021-09-29 23:26:28 +0900

    add cht

--- data-structure/convex_hull_trick.cpp
+++ data-structure/convex_hull_trick.cpp
@@ -3,14 +3,13 @@
 #include <utility>
 #include <vector>
 
-/*
- * @brief Convex Hull Trick
- */
 template <typename T>
 class ConvexHullTrick {
     using Line = std::pair<T, T>;
 
 public:
+    explicit ConvexHullTrick(bool monotone_query = false) : monotone_query(monotone_query) {}
+
     void add(T a, T b) {
         Line line(a, b);
         while (lines.size() >= 2 && check(*(lines.end() - 2), lines.back(), line)) {
@@ -20,22 +19,34 @@ public:
     }
 
     T get(T x) {
-        int lb = 0, ub = lines.size() - 1;
-        while (ub - lb > 1) {
-            int m = (lb + ub) / 2;
-            if (f(m, x) > f(m + 1, x))) lb = m;
-            else ub = m;
+        if (monotone_query) {
+            while (lines.size() - head >= 2 && f(head, x) > f(head + 1, x)) {
+                ++head;
+            }
+            return f(head, x);
+        } else {
+            int lb = -1, ub = lines.size() - 1;
+            while (ub - lb > 1) {
+                int m = (lb + ub) / 2;
+                if (f(m, x) > f(m + 1, x)) {
+                    lb = m;
+                } else {
+                    ub = m;
+                }
+            }
+            return f(ub, x);
         }
-        return f(ub, x);
     }
 
 private:
     std::vector<Line> lines;
+    bool monotone_query;
+    int head = 0;
 
-    bool check(const Line& l1, const Line& l2, const Line& l3) {
+    bool check(Line l1, Line l2, Line l3) {
         if (l1 > l3) std::swap(l1, l3);
-        return (l3.first - l1.first) * (l2.second - l1.second) >= (l2.first - l1.first) * (l3.second - l1.second);
-
+        return (l3.first - l1.first) * (l2.second - l1.second) >=
+               (l2.first - l1.first) * (l3.second - l1.second);
     }
 
     inline T f(int i, T x) {
