c9192b86e27a8ae3d1881c41532d06ca8373db82
Sota Nishiyama <sotanishy@Adansonss-MacBook-Pro-Sota.local>
2022-10-10 09:58:59 +0900

    add boyer-moore majority vote algo

---
+++ misc/majority.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <optional>
+#include <vector>
+
+template <typename T>
+std::optional<T> majority(const std::vector<T>& v) {
+    T m;
+    int cnt = 0;
+    for (auto x : v) {
+        if (cnt == 0) {
+            m = x;
+            ++cnt;
+        } else if (m == x) {
+            ++cnt;
+        } else {
+            --cnt;
+        }
+    }
+    cnt = 0;
+    for (auto x : v) {
+        if (m == x) {
+            ++cnt;
+        }
+    }
+    return cnt > (int) v.size() / 2 ? std::optional<T>(m) : std::nullopt;
+}
