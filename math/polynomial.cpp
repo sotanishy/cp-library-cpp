5e592be43ac414456b5163d748cdb1ea0a49f136
Sota Nishiyama <2480souta@gmail.com>
2023-03-10 20:58:06 +0900

    update polynomial and add multipoint evaluation

--- math/polynomial.cpp
+++ math/polynomial.cpp
@@ -2,39 +2,41 @@
 #include <algorithm>
 #include <cassert>
 #include <vector>
+
 #include "../convolution/ntt.hpp"
 
 template <typename mint>
 class Polynomial : public std::vector<mint> {
     using Poly = Polynomial;
 
-public:
+   public:
     using std::vector<mint>::vector;
     using std::vector<mint>::operator=;
 
-    Poly pre(int size) const { return Poly(this->begin(), this->begin() + std::min((int) this->size(), size)); }
+    Poly pre(int size) const {
+        return Poly(this->begin(),
+                    this->begin() + std::min((int)this->size(), size));
+    }
 
     Poly rev(int deg = -1) const {
-        Poly ret(*this);
+        auto ret = *this;
         if (deg != -1) ret.resize(deg, 0);
         return Poly(ret.rbegin(), ret.rend());
     }
 
-    Poly& operator+=(const Poly& rhs) {
-        if (this->size() < rhs.size()) this->resize(rhs.size());
-        for (int i = 0; i < (int) rhs.size(); ++i) (*this)[i] += rhs[i];
-        return *this;
+    // --- unary operation ---
+
+    Poly& operator-() const {
+        auto ret = *this;
+        for (auto& x : ret) x = -x;
+        return ret;
     }
+
+    // -- binary operation with constant
 
     Poly& operator+=(const mint& rhs) {
         if (this->empty()) this->resize(1);
         (*this)[0] += rhs;
-        return *this;
-    }
-
-    Poly& operator-=(const Poly& rhs) {
-        if (this->size() < rhs.size()) this->resize(rhs.size());
-        for (int i = 0; i < (int) rhs.size(); ++i) (*this)[i] -= rhs[i];
         return *this;
     }
 
@@ -44,26 +46,47 @@ public:
         return *this;
     }
 
+    Poly& operator*=(const mint& rhs) {
+        for (auto& x : *this) x *= rhs;
+        return *this;
+    }
+
+    Poly& operator/=(const mint& rhs) { return *this *= rhs.inv(); }
+
+    Poly operator+(const mint& rhs) const { return Poly(*this) += rhs; }
+    Poly operator-(const mint& rhs) const { return Poly(*this) -= rhs; }
+    Poly operator*(const mint& rhs) const { return Poly(*this) *= rhs; }
+    Poly operator/(const mint& rhs) const { return Poly(*this) /= rhs; }
+
+    // --- binary operation with polynomial ---
+
+    Poly& operator+=(const Poly& rhs) {
+        if (this->size() < rhs.size()) this->resize(rhs.size());
+        for (int i = 0; i < (int)rhs.size(); ++i) (*this)[i] += rhs[i];
+        return *this;
+    }
+
+    Poly& operator-=(const Poly& rhs) {
+        if (this->size() < rhs.size()) this->resize(rhs.size());
+        for (int i = 0; i < (int)rhs.size(); ++i) (*this)[i] -= rhs[i];
+        return *this;
+    }
+
     Poly& operator*=(const Poly& rhs) {
         *this = convolution(*this, rhs);
+        return *this;
         // // naive convolution O(N^2)
         // std::vector<mint> res(this->size() + rhs.size() - 1);
-        // for (int i = 0; i < (int) this->size(); ++i) {
-        //     for (int j = 0; j < (int) rhs.size(); ++j) {
+        // for (int i = 0; i < (int)this->size(); ++i) {
+        //     for (int j = 0; j < (int)rhs.size(); ++j) {
         //         res[i + j] += (*this)[i] * rhs[j];
         //     }
         // }
-        // *this = res;
-        return *this;
-    }
-
-    Poly& operator*=(const mint& rhs) {
-        for (int i = 0; i < (int) this->size(); ++i) (*this)[i] *= rhs;
-        return *this;
+        // return *this = res;
     }
 
     Poly& operator/=(const Poly& rhs) {
-        if(this->size() < rhs.size()) {
+        if (this->size() < rhs.size()) {
             this->clear();
             return *this;
         }
@@ -73,64 +96,83 @@ public:
 
     Poly& operator%=(const Poly& rhs) {
         *this -= *this / rhs * rhs;
-        while (!this->empty() && this->back() == 0) this->pop_back();
+        while ((int)this->size() > 1 && this->back() == 0) this->pop_back();
         return *this;
     }
 
-    Poly& operator-() const {
-        Poly ret(this->size());
-        for (int i = 0; i < (int) this->size(); ++i) ret[i] = -(*this)[i];
-        return ret;
-    }
-
     Poly operator+(const Poly& rhs) const { return Poly(*this) += rhs; }
-    Poly operator+(const mint& rhs) const { return Poly(*this) += rhs; }
     Poly operator-(const Poly& rhs) const { return Poly(*this) -= rhs; }
-    Poly operator-(const mint& rhs) const { return Poly(*this) -= rhs; }
     Poly operator*(const Poly& rhs) const { return Poly(*this) *= rhs; }
-    Poly operator*(const mint& rhs) const { return Poly(*this) *= rhs; }
     Poly operator/(const Poly& rhs) const { return Poly(*this) /= rhs; }
     Poly operator%(const Poly& rhs) const { return Poly(*this) %= rhs; }
 
+    // --- shift operation ---
+
     Poly operator<<(int n) const {
-        Poly ret(*this);
+        auto ret = *this;
         ret.insert(ret.begin(), n, 0);
         return ret;
     }
 
     Poly operator>>(int n) const {
-        if (this->size() <= n) return {};
-        Poly ret(*this);
+        if ((int)this->size() <= n) return {};
+        auto ret = *this;
         ret.erase(ret.begin(), ret.begin() + n);
         return ret;
     }
 
+    // --- evaluation ---
 
     mint operator()(const mint& x) {
         mint y = 0, powx = 1;
-        for (int i = 0; i < (int) this->size(); ++i) {
-            y += (*this)[i] * powx;
-            powx *= x;
+        for (int i = 0; i < (int)this->size(); ++i) {
+            for (auto c : *this) {
+                y += c * powx;
+                powx *= x;
+            }
+            return y;
         }
-        return y;
     }
+
+    // --- other operations ---
 
     Poly inv(int deg = -1) const {
         assert((*this)[0] != mint(0));
         if (deg == -1) deg = this->size();
-        Poly ret({mint(1) / (*this)[0]});
-        for (int i = 1; i < deg; i <<= 1) {
-            ret = (ret * mint(2) - ret * ret * this->pre(i << 1)).pre(i << 1);
+        Poly res = {(*this)[0].inv()};
+        for (int d = 1; d < deg; d <<= 1) {
+            auto f = pre(2 * d);
+            auto g = res;
+            f.resize(2 * d);
+            g.resize(2 * d);
+
+            // g_{n+1} = g_n * (2 - g_n * f) mod x^{2^{n+1}}
+
+            ntt(f);
+            ntt(g);
+            for (int i = 0; i < 2 * d; ++i) f[i] *= g[i];
+            intt(f);
+
+            for (int i = 0; i < d; ++i) f[i] = 0;
+
+            ntt(f);
+            for (int i = 0; i < 2 * d; ++i) f[i] *= g[i];
+            intt(f);
+
+            res.resize(2 * d);
+            auto coef = mint(2 * d).inv().pow(2);
+            for (int i = d; i < 2 * d; ++i) res[i] = -f[i] * coef;
         }
-        return ret;
+        return res.pre(deg);
     }
 
     Poly exp(int deg = -1) const {
         assert((*this)[0] == mint(0));
         if (deg == -1) deg = this->size();
-        Poly ret({mint(1)});
+        Poly ret = {mint(1)};
         for (int i = 1; i < deg; i <<= 1) {
-            ret = (ret * (this->pre(i << 1) + mint(1) - ret.log(i << 1))).pre(i << 1);
+            ret = (ret * (this->pre(i << 1) + mint(1) - ret.log(i << 1)))
+                      .pre(i << 1);
         }
         return ret;
     }
@@ -138,45 +180,47 @@ public:
     Poly log(int deg = -1) const {
         assert((*this)[0] == mint(1));
         if (deg == -1) deg = this->size();
-        return (this->diff() * this->inv(deg)).pre(deg - 1).integral();
+        return (diff() * inv(deg)).pre(deg - 1).integral();
     }
 
     Poly pow(long long k, int deg = -1) const {
         if (k == 0) return {1};
         if (deg == -1) deg = this->size();
-        Poly ret(*this);
-        int cnt = 0;
-        while (cnt < (int) ret.size() && ret[cnt] == mint(0)) ++cnt;
-        if (cnt * k >= deg) return Poly(deg, mint(0));
-        ret.erase(ret.begin(), ret.begin() + cnt);
-        deg -= cnt * k;
-        ret = ((ret * mint(ret[0]).inv()).log(deg) * mint(k)).pre(deg).exp(deg) * mint(ret[0]).pow(k);
-        ret.insert(ret.begin(), cnt * k, mint(0));
-        return ret;
+        auto ret = *this;
+        int cnt0 = 0;
+        while (cnt0 < (int)ret.size() && ret[cnt0] == 0) ++cnt0;
+        if (cnt0 > (deg - 1) / k) return {};
+        ret = ret >> cnt0;
+        deg -= cnt0 * k;
+        ret = ((ret / ret[0]).log(deg) * k).exp(deg) * ret[0].pow(k);
+        ret = ret << (cnt0 * k);
+        return ret.deg();
     }
 
     Poly diff() const {
-        Poly ret(std::max(0, (int) this->size() - 1));
-        for (int i = 1; i <= (int) ret.size(); ++i) ret[i - 1] = (*this)[i] * mint(i);
+        Poly ret(std::max(0, (int)this->size() - 1));
+        for (int i = 1; i <= (int)ret.size(); ++i)
+            ret[i - 1] = (*this)[i] * mint(i);
         return ret;
     }
 
     Poly integral() const {
         Poly ret(this->size() + 1);
         ret[0] = mint(0);
-        for (int i = 0; i < (int) ret.size() - 1; ++i) ret[i + 1] = (*this)[i] / mint(i + 1);
+        for (int i = 0; i < (int)ret.size() - 1; ++i)
+            ret[i + 1] = (*this)[i] / mint(i + 1);
         return ret;
     }
 
     Poly taylor_shift(long long c) const {
         const int n = this->size();
         std::vector<mint> fact(n, 1), fact_inv(n, 1);
-        for (int i = 1; i < n; ++i) fact[i] = fact[i-1] * i;
-        fact_inv[n-1] = mint(1) / fact[n-1];
-        for (int i = n - 1; i > 0; --i) fact_inv[i-1] = fact_inv[i] * i;
+        for (int i = 1; i < n; ++i) fact[i] = fact[i - 1] * i;
+        fact_inv[n - 1] = mint(1) / fact[n - 1];
+        for (int i = n - 1; i > 0; --i) fact_inv[i - 1] = fact_inv[i] * i;
 
         auto ret = *this;
-        Poly e(n+1);
+        Poly e(n + 1);
         e[0] = 1;
         mint p = c;
         for (int i = 1; i < n; ++i) {
--- test/yosupo/pow_of_formal_power_series.test.cpp
+++ test/yosupo/pow_of_formal_power_series.test.cpp
@@ -1,10 +1,11 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/inv_of_formal_power_series"
+#define PROBLEM "https://judge.yosupo.jp/problem/pow_of_formal_power_series"
 
 #include "../../math/modint.cpp"
 #include "../../math/polynomial.cpp"
 
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 using mint = Modint<998244353>;
 
@@ -13,9 +14,11 @@ int main() {
     cin.tie(nullptr);
 
     int N;
-    cin >> N;
+    ll M;
+    cin >> N >> M;
     Polynomial<mint> f(N);
     for (int i = 0; i < N; ++i) cin >> f[i];
-    auto g = f.inv(N);
+    auto g = f.pow(M);
+    g.resize(N);
     for (int i = 0; i < N; ++i) cout << g[i] << (i < N - 1 ? " " : "\n");
 }
