0551af5e7ed47d6119fd21ba87aecb66e48af869
Sota Nishiyama <2480souta@gmail.com>
2023-03-15 19:34:34 +0900

    add xor segment tree

---
+++ data-structure/segtree/xor_segment_tree.hpp
@@ -0,0 +1,100 @@
+#pragma once
+#include <algorithm>
+#include <vector>
+
+template <typename M>
+class XorSegmentTree {
+    using T = typename M::T;
+
+   public:
+    XorSegmentTree() = default;
+    explicit XorSegmentTree(int n)
+        : XorSegmentTree(std::vector<T>(n, M::id())) {}
+    explicit XorSegmentTree(const std::vector<T>& v) {
+        n = 0, size = 1;
+        while (size < (int)v.size()) size <<= 1, ++n;
+        table.resize(2 * size);
+
+        for (int k = 0; k < size; ++k) {
+            table[size + k].resize(1, k < (int)v.size() ? v[k] : M::id());
+        }
+        half = 1 << ((n + 1) / 2);
+
+        for (int k = size - 1; k >= half; --k) pull(k);
+    }
+
+    void update(int k, const T& x) {
+        k += size;
+        table[k][0] = x;
+        for (k >>= 1; k >= half; k >>= 1) pull(k);
+    }
+
+    T fold(int l, int r, int x) const {
+        T vl = M::id(), vr = M::id();
+        int i = 0;
+        for (l += size, r += size; l < r && l > 2 * half;
+             l >>= 1, r >>= 1, ++i) {
+            if (l & 1)
+                vl = M::op(vl, table[l++ ^ (x >> i)][x & ((1 << i) - 1)]);
+            if (r & 1)
+                vr = M::op(table[--r ^ (x >> i)][x & ((1 << i) - 1)], vr);
+        }
+        for (int k = l; k < r; ++k) {
+            vl = M::op(vl, table[k ^ (x >> i)][x & ((1 << i) - 1)]);
+        }
+        return M::op(vl, vr);
+    }
+
+   private:
+    int n, size, half;
+    std::vector<std::vector<T>> table;
+
+    void pull(int k) {
+        int i = n - (31 - __builtin_clz(k));
+        table[k].resize(1 << i);
+        for (int x = 0; x < (1 << i); ++x) {
+            T vl = table[2 * k][x & ~(1 << (i - 1))];
+            T vr = table[2 * k + 1][x & ~(1 << (i - 1))];
+            if (x >> (i - 1) & 1) std::swap(vl, vr);
+            table[k][x] = M::op(vl, vr);
+        }
+    }
+};
+
+template <typename M>
+class XorSegmentTreeCommutative {
+    using T = typename M::T;
+
+   public:
+    XorSegmentTreeCommutative() = default;
+    explicit XorSegmentTreeCommutative(int n)
+        : XorSegmentTreeCommutative(std::vector<T>(n, M::id())) {}
+    explicit XorSegmentTreeCommutative(const std::vector<T>& v) {
+        size = 1;
+        while (size < (int)v.size()) size <<= 1;
+        node.resize(2 * size, M::id());
+        std::copy(v.begin(), v.end(), node.begin() + size);
+
+        for (int k = size - 1; k > 0; --k)
+            node[k] = M::op(node[2 * k], node[2 * k + 1]);
+    }
+
+    void update(int k, const T& x) {
+        k += size;
+        node[k] = x;
+        while (k >>= 1) node[k] = M::op(node[2 * k], node[2 * k + 1]);
+    }
+
+    T fold(int l, int r, int x) const {
+        T res = M::id();
+        for (l += size, r += size; l < r; l >>= 1, r >>= 1, x >>= 1) {
+            if (l & 1) res = M::op(res, node[l++ ^ x]);
+            if (r & 1) res = M::op(node[--r ^ x], res);
+        }
+        return res;
+    }
+
+   private:
+    int size;
+    std::vector<T> node;
+};
---
+++ docs/data-structure/segtree/xor_segment_tree.md
@@ -0,0 +1,29 @@
+---
+title: Xor Segment Tree
+documentation_of: ../../../data-structure/segtree/xor_segment_tree.hpp
+---
+
+## Description
+
+XOR セグメント木は，通常のセグメント木の操作の際に， index をすべて非負整数 $x$ との xor を適用したものに置き換えて操作することができる．
+
+空間計算量: $O(n \log n)$
+
+## Operations
+
+- `XorSegmentTree(int n)`
+    - サイズ`n`で要素がすべて単位元 $e$ のセグメント木を構築する
+    - 時間計算量: $O(n\log n)$
+- `SegmentTree(vector<T> v)`
+    - `v`の要素からサイズ`n = v.size()`のセグメント木を構築する
+    - 時間計算量: $O(n\log n)$
+- `void update(int k, T x)`
+    - $k$ 番目の要素を $x$ に更新する
+    - 時間計算量: $O(\sqrt{n})$
+- `T fold(int l, int r, int x)`
+    - $a_{l\oplus x} * a_{(l+1)\oplus x} * \dots * a_{(r-1) \oplus x}$ を計算する
+    - 時間計算量: $O(\sqrt{n})$
+
+## Note
+
+モノイドが可換であるバージョン `XorSegmentTreeCommutative` は，通常のセグメント木と同じ計算量で各種操作を実行することができる．
