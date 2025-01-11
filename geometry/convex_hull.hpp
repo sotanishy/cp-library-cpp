9ca8c1d0f39f7fef0c8e925e460d81302851b2cf
Sota Nishiyama <Sota Nishiyama>
2025-01-11 18:51:05 +0900

    fix convex hull

--- geometry/convex_hull.hpp
+++ geometry/convex_hull.hpp
@@ -7,7 +7,9 @@ std::vector<Vec> convex_hull(std::vector<Vec> pts, bool strict = true) {
     std::ranges::sort(pts, {}, [](const Vec& v) {
         return std::make_pair(v.imag(), v.real());
     });
-    pts.erase(std::ranges::unique(pts).begin(), pts.end());
+    if (strict) {
+        pts.erase(std::ranges::unique(pts).begin(), pts.end());
+    }
     const int n = pts.size();
     if (n <= 1) return pts;
     int k = 0;  // the number of vertices in the convex hull
@@ -28,9 +30,15 @@ std::vector<Vec> convex_hull(std::vector<Vec> pts, bool strict = true) {
     int t = k;
     // left
     for (int i = n - 2; i >= 0; --i) {
-        while (k > t &&
-               leq(cross(ch[k - 1] - ch[k - 2], pts[i] - ch[k - 1]), 0))
-            --k;
+        if (strict) {
+            while (k > t &&
+                   leq(cross(ch[k - 1] - ch[k - 2], pts[i] - ch[k - 1]), 0))
+                --k;
+        } else {
+            while (k > t &&
+                   lt(cross(ch[k - 1] - ch[k - 2], pts[i] - ch[k - 1]), 0))
+                --k;
+        }
         ch[k++] = pts[i];
     }
     ch.resize(k - 1);
